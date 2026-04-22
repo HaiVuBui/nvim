98c9908f0348cf44f5d3b5e3746b7df7cde7c5f1
HaiVuBui <buivuhai1105@gmail.com>
2026-04-22 10:29:03 +0700

    terminal on the left, size 1/4

---
+++ lua/plugins/terminal.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "folke/snacks.nvim",
+    opts = {
+      terminal = {
+        win = {
+          position = "left",
+          width = 0.25,
+        },
+      },
+    },
+  },
+}
