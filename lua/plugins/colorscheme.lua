d05da1ac655896f9675f3957ef9ba9b597b255ad
HaiVuBui <buivuhai1105@gmail.com>
2026-06-02 13:53:11 +0700

    cat latte

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -3,9 +3,16 @@ return {
   { "kepano/flexoki-neovim", name = "flexoki" },
   { "olimorris/onedarkpro.nvim" },
   {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    opts = {
+      flavour = "latte",
+    },
+  },
+  {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "onelight",
+      colorscheme = "catppuccin-latte",
     },
   },
 }
